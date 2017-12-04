d5684e6c07fcb699c84b6fbafb93ce40ac6958a1
Oleg Kosmakov <kosmakoff@gmail.com>
2017-12-04 13:08:06 +0100

    Day 01 complete

---
+++ Common/LinqExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> SlidingWindow<T>(this IEnumerable<T> source, int windowLength)
+        {
+            if (windowLength < 1)
+                throw new ArgumentOutOfRangeException(nameof(windowLength), "Must be greater or equal to 1");
+
+            Queue<T> buffer = new Queue<T>(windowLength);
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    if (buffer.Count == windowLength)
+                    {
+                        yield return buffer.ToArray();
+                        buffer.Dequeue();
+                    }
+
+                    buffer.Enqueue(enumerator.Current);
+                }
+
+                if (buffer.Count == windowLength)
+                {
+                    yield return buffer.ToArray();
+                }
+            }
+        }
+    }
+}
