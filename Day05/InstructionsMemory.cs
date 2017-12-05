2a379c5adf15e14b7f79e885be4b9f4ce20c0b20
Oleg Kosmakov <kosmakoff@gmail.com>
2017-12-05 11:41:57 +0100

    Day 05 Complete

---
+++ Day05/InstructionsMemory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day05
+{
+    internal class InstructionsMemory
+    {
+        private readonly bool _isStrange;
+        private readonly int[] _instructions;
+        private int _pointer;
+
+        public InstructionsMemory(int[] instructions, bool isStrange = false)
+        {
+            _isStrange = isStrange;
+            _instructions = new int[instructions.Length];
+            Array.Copy(instructions, _instructions, instructions.Length);
+        }
+
+        public InstructionsMemory(string fileName, bool isStrange = false)
+            : this(File.ReadAllLines(fileName).Select(int.Parse).ToArray(), isStrange)
+        {
+        }
+
+        public bool FollowInstruction()
+        {
+            var offset = _instructions[_pointer];
+
+            if (_isStrange && offset >= 3)
+                _instructions[_pointer]--;
+            else
+                _instructions[_pointer]++;
+
+            _pointer += offset;
+            return _pointer < 0 || _pointer >= _instructions.Length;
+        }
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,38 @@
+﻿using static Common.Utils;
+
+namespace Day05
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PrintHeader("Day 05");
+
+            // var instructionsMemory = new InstructionsMemory(new[] { 0, 3, 0, 1, -3 }, isStrange: false);
+            var instructionsMemory = new InstructionsMemory("Input.txt", isStrange: false);
+
+            var steps = 0;
+
+            while (!instructionsMemory.FollowInstruction())
+                steps++;
+
+            // count last step which led to exit
+            steps++;
+
+            PrintAnswer("Problem 1 answer", steps);
+
+            // instructionsMemory = new InstructionsMemory(new[] { 0, 3, 0, 1, -3 }, isStrange: true);
+            instructionsMemory = new InstructionsMemory("Input.txt", isStrange: true);
+
+            steps = 0;
+
+            while (!instructionsMemory.FollowInstruction())
+                steps++;
+
+            // count last step which led to exit
+            steps++;
+
+            PrintAnswer("Problem 2 answer", steps);
+        }
+    }
+}
