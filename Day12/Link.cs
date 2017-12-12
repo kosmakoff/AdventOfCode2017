9061a20d02d73c5f2c9721c8d3be62f98c15bfc0
Oleg Kosmakov <kosmakoff@gmail.com>
2017-12-12 12:24:24 +0100

    Day 12 complete

---
+++ Day12/Link.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day12
+{
+    internal class Link
+    {
+        private static readonly Regex LinkRegex = new Regex(@"^(?<id>\d+) \<-\> (?<ids>\d+(, \d+)*)$");
+
+        public int Id { get; }
+
+        public IList<int> LinkedIds { get; }
+
+        public Link(int id, IList<int> linkedIds)
+        {
+            Id = id;
+            LinkedIds = linkedIds;
+        }
+
+        public static Link Parse(string linkString)
+        {
+            var match = LinkRegex.Match(linkString);
+            if (!match.Success)
+                throw new FormatException($"'{linkString}' is not in valid format");
+
+            var id = int.Parse(match.Groups["id"].Value);
+            var ids = match.Groups["ids"].Value.Split(", ").Select(int.Parse).ToList();
+
+            return new Link(id, ids);
+        }
+    }
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Common.Utils;
+
+namespace Day12
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PrintHeader("Day 12");
+
+            var input = File.ReadAllLines("Input.txt");
+            var linksDictionary = input.Select(Link.Parse)
+                .ToDictionary(a => a.Id, a => a.LinkedIds);
+
+            var groups = new Dictionary<int, HashSet<int>>();
+
+            foreach (var id in linksDictionary.Keys)
+            {
+                if (groups.Values.Any(list => list.Contains(id)))
+                    continue;
+
+                var group = new HashSet<int> { id };
+                groups[id] = group;
+
+                var queue = new Queue<int>();
+                queue.Enqueue(id);
+
+                while (queue.TryDequeue(out var subId))
+                {
+                    var moreIds = linksDictionary[subId];
+
+                    group.Add(subId);
+
+                    foreach (var moreId in moreIds)
+                    {
+                        if (!group.Contains(moreId))
+                            queue.Enqueue(moreId);
+                    }
+                }
+            }
+
+            PrintAnswer("Problem 1 answer", groups[0].Count);
+            PrintAnswer("Problem 2 answer", groups.Count);
+        }
+    }
+}
