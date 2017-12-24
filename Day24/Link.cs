71f308505a163879528b937f47860f5392da03dc
Oleg Kosmakov <kosmakoff@gmail.com>
2017-12-24 15:13:41 +0100

    Day 24 complete

---
+++ Day24/Link.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day24
+{
+    internal class Link
+    {
+        public Guid Guid { get; }
+
+        private Link(int leftSide, int rightSide, Guid guid)
+        {
+            Guid = guid;
+            LeftSide = leftSide;
+            RightSide = rightSide;
+        }
+
+        public Link(int leftSide, int rightSide)
+            : this(leftSide, rightSide, Guid.NewGuid())
+        {
+        }
+
+        public int LeftSide { get; }
+        public int RightSide { get; }
+
+        public static Link Parse(string link)
+        {
+            var parts = link.Split('/');
+
+            return new Link(int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+
+        public Link Rotate(int side)
+        {
+            return LeftSide == side ? this : new Link(RightSide, LeftSide, Guid);
+        }
+
+        public override string ToString()
+        {
+            return $"{LeftSide}/{RightSide}";
+        }
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Common.Utils;
+
+namespace Day24
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            PrintHeader("Day 24");
+
+            var input = File.ReadAllLines("Input.txt");
+            var links = input.Select(Link.Parse).ToList();
+
+            var maxBridgeWeight = EnumerateBridges(0, links)
+                .Select(a => a.Sum(l => l.LeftSide + l.RightSide))
+                .Max();
+
+            PrintAnswer("Problem 1 answer", maxBridgeWeight);
+
+            var maxLengthMaxBridgeWeight = EnumerateBridges(0, links)
+                .Select(bridge => bridge.ToList())
+                .Select(a => new {Length = a.Count, Weight = a.Sum(link => link.LeftSide + link.RightSide)})
+                .GroupBy(a => a.Length, a => a.Weight)
+                .OrderByDescending(grp => grp.Key)
+                .First()
+                .Max();
+
+            PrintAnswer("Problem 2 answer", maxLengthMaxBridgeWeight);
+        }
+
+        private static IEnumerable<IEnumerable<Link>> EnumerateBridges(int start, List<Link> pool)
+        {
+            var roots = pool
+                .Where(link => link.LeftSide == start || link.RightSide == start)
+                .Select(link => link.Rotate(start));
+
+            foreach (var root in roots)
+            {
+                yield return new[] {root};
+
+                var subBridges = EnumerateBridges(root.RightSide, pool.Where(l => l.Guid != root.Guid).ToList());
+                foreach (var subBridge in subBridges)
+                {
+                    yield return new[] {root}.Concat(subBridge);
+                }
+            }
+        }
+    }
+}
